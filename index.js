977a76b19ab7ac75eccc1bad1fa97386d94740d7
Jacob Buck <buck.jacob@gmail.com>
2017-03-12 18:12:41 +1300

    Tabs to 2 spaces

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 'use strict';
 
 module.exports = function rafq() {
-	var queue = [];
-	var raf;
+  var queue = [];
+  var raf;
 
-	function tick(time) {
-		var prevQueue = queue;
-		queue = [];
-		prevQueue.forEach(function(value) {
-			value(time);
-		});
-	}
+  function tick(time) {
+    var prevQueue = queue;
+    queue = [];
+    prevQueue.forEach(function(value) {
+      value(time);
+    });
+  }
 
-	return {
-		add: function(callback) {
-			var prevQueue = queue;
-			queue = queue.concat([callback]);
-			if (!prevQueue.length) {
-				raf = requestAnimationFrame(tick);
-			}
-			return this;
-		},
+  return {
+    add: function(callback) {
+      var prevQueue = queue;
+      queue = queue.concat([callback]);
+      if (!prevQueue.length) {
+        raf = requestAnimationFrame(tick);
+      }
+      return this;
+    },
 
-		clear: function() {
-			queue = [];
-			cancelAnimationFrame(raf);
-			return this;
-		},
+    clear: function() {
+      queue = [];
+      cancelAnimationFrame(raf);
+      return this;
+    },
 
-		remove: function(callback) {
-			queue = queue.filter(function(value) {
-				return value !== callback;
-			});
-			if (!queue.length) {
-				cancelAnimationFrame(raf);
-			}
-			return this;
-		}
-	};
+    remove: function(callback) {
+      queue = queue.filter(function(value) {
+        return value !== callback;
+      });
+      if (!queue.length) {
+        cancelAnimationFrame(raf);
+      }
+      return this;
+    }
+  };
 };
